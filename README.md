7d159f88597429077d573a07357f1fa5d98caa5e
Jarek Karwowski <chromedivizer@gmail.com>
2024-08-11 11:21:23 +0200

    [#2] added visualization markers for individual humans

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Provides ROS node for visualization of data processed by [`people_msgs_utils`](h
 
 Implemented nodes:
 
-* `visualization_node` - creates markers related to human groups and their members. Currently, when a human is not assigned to any group, it will not be visualized (see [#2](https://github.com/rayvburn/people_msgs_utils_vis/issues/2)).
+* `visualization_node` - creates markers related to human individuals as well as human groups and their members.
--- src/visualization_node.cpp
+++ src/visualization_node.cpp
@@ -6,6 +6,180 @@
 // global publisher for markers
 ros::Publisher pub;
 
+// individuals
+visualization_msgs::MarkerArray createMarkers(
+	const people_msgs_utils::People& people,
+	const std::string& frame_id
+) {
+	visualization_msgs::MarkerArray marker_array;
+
+	// default color with no transparency at all
+	std_msgs::ColorRGBA color;
+	color.a = 1.0;
+	color.r = 1.0;
+	color.g = 1.0;
+	color.b = 1.0;
+
+	// default header
+	std_msgs::Header header;
+	header.frame_id = frame_id;
+	header.stamp = ros::Time::now();
+
+	// default value of the marker's lifetime
+	const ros::Duration MARKER_LIFETIME(1.0);
+
+	// lambdas creating subsequent marker types
+	auto create_shape_marker_fun = [=](
+		const people_msgs_utils::Person& person,
+		int person_id
+	) -> visualization_msgs::Marker {
+		visualization_msgs::Marker marker;
+		marker.header = header;
+		marker.ns = "individuals/shape";
+		marker.type = visualization_msgs::Marker::CYLINDER;
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.lifetime = MARKER_LIFETIME;
+		marker.id = person_id;
+		marker.pose = person.getPose();
+		// scale of the shape
+		marker.scale.x = 0.55;
+		marker.scale.y = 0.55;
+		marker.scale.z = 1.75;
+		// adjust height (let the bottom be at the ground); must be executed once the scale is updated
+		marker.pose.position.z += marker.scale.z / 2;
+
+		marker.color = color;
+		// will fade to a fully transparent marker when reliability becomes 0
+		marker.color.a = person.getReliability();
+		return marker;
+	};
+
+	auto create_text_marker_fun = [=](
+		const people_msgs_utils::Person& person,
+		int person_id,
+		double shape_height,
+		double shape_width
+	) -> visualization_msgs::Marker {
+		visualization_msgs::Marker marker;
+		marker.header = header;
+		marker.ns = "individuals/text";
+		marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.lifetime = MARKER_LIFETIME;
+		marker.id = person_id;
+		marker.pose = person.getPose();
+		// adjust height (let the bottom of text be above the marker)
+		marker.pose.position.z = 1.2 * shape_height;
+		// scale of the text
+		marker.scale.z = shape_width;
+
+		marker.color = color;
+		// will fade to a fully transparent marker when reliability becomes 0
+		marker.color.a = person.getReliability();
+		// let the marker show provided name of the person
+		marker.text = person.getName();
+		return marker;
+	};
+
+	auto create_orientation_marker_fun = [=](
+		const people_msgs_utils::Person& person,
+		int person_id,
+		double shape_height,
+		double shape_depth
+	) -> visualization_msgs::Marker {
+		visualization_msgs::Marker marker;
+		marker.header = header;
+		marker.ns = "individuals/orientation";
+		marker.type = visualization_msgs::Marker::ARROW;
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.lifetime = MARKER_LIFETIME;
+		marker.id = person_id;
+		marker.pose = person.getPose();
+		// adjust height (let the bottom of the arrow be above the shape)
+		marker.pose.position.z = shape_height;
+		// scale of the arrow
+		marker.scale.x = shape_depth;
+		marker.scale.y = 0.1;
+		marker.scale.z = 0.1;
+
+		marker.color = color;
+		// will fade to a fully transparent marker when reliability becomes 0
+		marker.color.a = person.getReliability();
+		return marker;
+	};
+
+	auto create_velocity_marker_fun = [=](
+		const people_msgs_utils::Person& person,
+		int person_id,
+		double shape_depth
+	) -> visualization_msgs::Marker {
+		visualization_msgs::Marker marker;
+		marker.header = header;
+		marker.ns = "individuals/velocity";
+		marker.type = visualization_msgs::Marker::ARROW;
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.lifetime = MARKER_LIFETIME;
+		marker.id = person_id;
+		marker.pose.position = person.getPosition();
+		// adjust height (let the bottom of the arrow be at the ground level)
+		marker.pose.position.z = 0.0;
+
+		// set marker's orientation according to the velocity of a person
+		tf2::Quaternion vel_orientation;
+		// NOTE: for a proper yaw angle, yaw, pitch and roll angles must be reordered (compared to the documentation)
+		vel_orientation.setEuler(
+			0.0,
+			0.0,
+			std::atan2(person.getVelocityY(), person.getVelocityX())
+		);
+		marker.pose.orientation = tf2::toMsg(vel_orientation);
+
+		// scale of the arrow
+		// velocity value based on "Moussaid et al., Experimental study (...), 2009"
+		const double VEL_MAX = 1.29;
+		double vel_magnitude = std::hypot(person.getVelocityX(), person.getVelocityY());
+		double vel_percentage = std::min(vel_magnitude / VEL_MAX, 1.0);
+		// shape_depth offset so the vel. vector is visible in front of the "shape"
+		marker.scale.x = 0.5 * shape_depth + vel_percentage;
+		marker.scale.y = 0.1;
+		marker.scale.z = 0.1;
+
+		marker.color = color;
+		// will fade to a fully transparent marker when reliability becomes 0
+		marker.color.a = person.getReliability();
+		return marker;
+	};
+
+	for (const auto& person: people) {
+		int person_id = 0;
+		try {
+			person_id = std::stoi(person.getName());
+		} catch (const std::invalid_argument& e) {
+			person_id = std::numeric_limits<int>::max();
+			ROS_ERROR(
+				"Could not convert '%s' to an integer! Assigned '%d' as a fallback person ID",
+				person.getName().c_str(),
+				person_id
+			);
+		}
+
+		// create markers
+		auto shape = create_shape_marker_fun(person, person_id);
+		auto text = create_text_marker_fun(person, person_id, shape.scale.z, shape.scale.y);
+		auto orientation = create_orientation_marker_fun(person, person_id, shape.scale.z, shape.scale.x);
+		auto vel = create_velocity_marker_fun(person, person_id, shape.scale.x);
+
+		// collect markers
+		marker_array.markers.push_back(shape);
+		marker_array.markers.push_back(text);
+		marker_array.markers.push_back(orientation);
+		marker_array.markers.push_back(vel);
+	}
+
+	return marker_array;
+}
+
+// groups
 visualization_msgs::MarkerArray createMarkers(
 	const people_msgs_utils::Groups& groups,
 	const std::string& frame_id
@@ -15,7 +189,7 @@ visualization_msgs::MarkerArray createMarkers(
 	for (const auto& grp: groups) {
 		visualization_msgs::Marker group_marker;
 		group_marker.action = visualization_msgs::Marker::ADD;
-		group_marker.ns = "groups";
+		group_marker.ns = "groups/shape";
 		group_marker.id = std::stoi(grp.getName());
 		group_marker.color.a = grp.getReliability();
 		group_marker.color.r = 0.1;
@@ -40,7 +214,7 @@ visualization_msgs::MarkerArray createMarkers(
 			member_marker.pose.position.z += 0.1;
 			member_marker.action = visualization_msgs::Marker::ADD;
 			member_marker.type = visualization_msgs::Marker::CYLINDER;
-			member_marker.ns = "members";
+			member_marker.ns = "groups/members";
 			member_marker.id = std::stoi(member.getName());
 			member_marker.color.a = member.getReliability();
 			member_marker.color.r = 0.9;
@@ -56,7 +230,7 @@ visualization_msgs::MarkerArray createMarkers(
 
 		visualization_msgs::Marker cog_marker;
 		cog_marker.action = visualization_msgs::Marker::ADD;
-		cog_marker.ns = "cog";
+		cog_marker.ns = "groups/cog";
 		cog_marker.id = std::stoi(grp.getName());
 		cog_marker.color.a = grp.getReliability();
 		cog_marker.color.r = 0.5;
@@ -93,9 +267,11 @@ void peopleCallback(const people_msgs::People& people) {
 	std::tie(ppl, grp) = people_msgs_utils::createFromPeople(people.people);
 	// create markers
 	auto markers_groups = createMarkers(grp, people.header.frame_id);
+	auto markers_individuals = createMarkers(ppl, people.header.frame_id);
 
 	// publish
 	pub.publish(markers_groups);
+	pub.publish(markers_individuals);
 }
 
 int main(int argc, char **argv) {
